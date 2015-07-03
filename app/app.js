843325736592f7ae34ff8d60ea689b87b2972bf2
Mehrdad <mehrdad.dastgir@tricorsenedi.com>
2015-07-03 12:00:38 +0800

    still setting up initial pre-requisits

---
+++ app/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by meewog on 7/3/15.
+ */
+var app = angular.module
+(
+    'report',
+    [
+
+    ]
+);
---
+++ app/controllers/mainController.js
@@ -0,0 +1,12 @@
+/**
+ * Created by meewog on 7/3/15.
+ */
+app.controller('MainController',
+    [
+        '$scope',
+        function ($scope)
+        {
+            console.log('we are ready')
+        }
+    ]
+);
